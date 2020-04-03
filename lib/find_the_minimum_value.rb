2bca8d2b1d7ece423e55a8a44095eb4561e20a4c
Ryan Meinzer <ryanmeinzer@ryanmeinzer.com>
2020-04-03 19:12:01 +0000

    Automatically backed up by Learn

--- lib/find_the_minimum_value.rb
+++ lib/find_the_minimum_value.rb
@@ -4,6 +4,7 @@ def find_min_value(array)
   # could also just use a high number like 100000000
   while count < array.length do
     if min_value == nil || min_value > array[count]
+    # if using something like 100000000, this nil conditional isn't needed
     min_value = array[count]
     end
     count += 1
